71275e83bf86c57b6324d9ee85e6296f15a942c4
Pierre <pierrre.blanc34400@gmail.com>
2026-01-06 23:50:32 +0100

    fix: resolve syntax error in OfficeViewer and add mammoth types

---
+++ client/src/types/mammoth.d.ts
@@ -0,0 +1,10 @@
+declare module 'mammoth' {
+    export interface MammothResult {
+        value: string;
+        messages: any[];
+    }
+    export interface MammothInput {
+        arrayBuffer: ArrayBuffer;
+    }
+    export function convertToHtml(input: MammothInput): Promise<MammothResult>;
+}
