e1018f3a47d2ed4d6495056b9563f9396e55968e
Pierre <pierrre.blanc34400@gmail.com>
2026-01-11 00:37:39 +0100

    fix(build): fix TextStyle named import for deployment

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -2,7 +2,7 @@ import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import Placeholder from '@tiptap/extension-placeholder'
 import Underline from '@tiptap/extension-underline'
-import TextStyle from '@tiptap/extension-text-style'
+import { TextStyle } from '@tiptap/extension-text-style'
 import Color from '@tiptap/extension-color'
 import Highlight from '@tiptap/extension-highlight'
 import {
