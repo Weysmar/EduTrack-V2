07e38857585016388175c3f316c83e1db11386a1
Vincent <intelli.vince@gmail.com>
2026-01-30 23:11:43 +0100

    fix(finance): fix dashboard button syntax and improve filter

--- client/src/store/uiStore.ts
+++ client/src/store/uiStore.ts
@@ -6,12 +6,18 @@ interface UIStore {
     toggleSidebar: () => void
     closeSidebar: () => void
     toggleCollapse: () => void
+    isBankModalOpen: boolean
+    openBankModal: () => void
+    closeBankModal: () => void
 }
 
 export const useUIStore = create<UIStore>((set) => ({
     isSidebarOpen: true,
     isCollapsed: false,
+    isBankModalOpen: false,
     toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
     closeSidebar: () => set({ isSidebarOpen: false }),
     toggleCollapse: () => set((state) => ({ isCollapsed: !state.isCollapsed })),
+    openBankModal: () => set({ isBankModalOpen: true }),
+    closeBankModal: () => set({ isBankModalOpen: false }),
 }))
